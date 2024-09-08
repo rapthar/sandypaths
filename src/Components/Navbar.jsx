6cb79396f02423854271c34fc823381477cb6578
Mncedisi Bhembe <rapthargfx@gmail.com>
2024-09-08 20:41:03 +0200

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -29,8 +29,9 @@ function Navbar(){
 
     return <Box w="full" boxShadow={ isscroll? 'md' : "sm"} position="fixed" bg="white" zIndex={1}  p="3px 0px">
             <Box h="60px" w="85%" m="auto" display="flex" alignItems="center" justifyContent="space-between">
-            <Link to="/">
-            <Image boxSize='190px' src="/images/sandypathlogo.png" alt="logo" />
+            <Link href="/">
+              <Image boxSize='190px' src="/images/sandypathlogo.png" alt="logo" />
+            </Link>
             </Link>
             <Box display="flex" gap="25px" alignItems="center">
                 <Link display="flex">
