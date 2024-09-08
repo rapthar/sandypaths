0bd9708b77dfd235b60f25cb29c9b48e93aa9bf9
Mncedisi Bhembe <rapthargfx@gmail.com>
2024-09-08 19:28:10 +0200

    Layout

--- next.config.js
+++ next.config.js
@@ -1,15 +1,24 @@
-const withStylus = require('@zeit/next-stylus')
-
 /** @type {import('next').NextConfig} */
-const nextConfig = withStylus({
+const nextConfig = {
   reactStrictMode: true,
   webpack(config) {
     config.module.rules.push({
       test: /\.styl$/,
-      use: ['style-loader', 'css-loader', 'stylus-loader']
+      use: [
+        'style-loader',
+        {
+          loader: 'css-loader',
+          options: {
+            modules: {
+              localIdentName: '[name]__[local]___[hash:base64:5]',
+            },
+          },
+        },
+        'stylus-loader'
+      ]
     })
     return config
   }
-})
+}
 
 module.exports = nextConfig
