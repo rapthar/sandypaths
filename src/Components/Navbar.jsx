ae799ed92c7a6a3495b56d3ac5fcbc84999f3a51
Mncedisi Bhembe <rapthargfx@gmail.com>
2024-09-08 20:43:35 +0200

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Box, Link, Image, Text, Button  } from "@chakra-ui/react";
+import { Box, Link, Image, Text, Button } from "@chakra-ui/react";
 import ModeEditOutlineOutlinedIcon from '@mui/icons-material/ModeEditOutlineOutlined';
 import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
@@ -6,9 +6,7 @@ import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import Signin from "./Signin";
 import { useEffect, useState } from "react";
 
-
-function Navbar(){
-
+function Navbar() {
     const [isscroll, setisscroll] = useState(false);
     function isScrolling() {
         if (window.scrollY > 80) {
@@ -20,46 +18,46 @@ function Navbar(){
     useEffect(() => {
         window.addEventListener("scroll", isScrolling);
         return () => {
-        window.removeEventListener("scroll");
+            window.removeEventListener("scroll", isScrolling);
         }
-    }, [])
+    }, []);
 
-    
-    const [isAuth, setAuth]= useState(false);
+    const [isAuth, setAuth] = useState(false);
 
-    return <Box w="full" boxShadow={ isscroll? 'md' : "sm"} position="fixed" bg="white" zIndex={1}  p="3px 0px">
+    return (
+        <Box w="full" boxShadow={isscroll ? 'md' : "sm"} position="fixed" bg="white" zIndex={1} p="3px 0px">
             <Box h="60px" w="85%" m="auto" display="flex" alignItems="center" justifyContent="space-between">
-            <Link href="/">
-              <Image boxSize='190px' src="/images/sandypathlogo.png" alt="logo" />
-            </Link>
-            </Link>
-            <Box display="flex" gap="25px" alignItems="center">
-                <Link display="flex">
-                    <ModeEditOutlineOutlinedIcon />
-                    <Text fontWeight="500" fontSize='md'>Review</Text>
+                <Link href="/">
+                    <Image boxSize='190px' src="/images/sandypathlogo.png" alt="logo" />
                 </Link>
-                <Link display="flex">
-                    <FavoriteBorderOutlinedIcon />
-                    <Text fontWeight="500" fontSize='md'>Trips</Text>
-                </Link>
-                <Link display="flex">
-                    <NotificationsOutlinedIcon />
-                    <Text fontWeight="500" fontSize='md'>Alerts</Text>
-                </Link>
-                <Link display="flex">
-                    {isAuth ? 
-                    <Button color="black" onClick={()=>{
-                        setAuth(!isAuth);
-                    }}>Sanghamitra</Button> : <Signin />
-                }
-                </Link>
-                <Link display="flex">
-                    <ShoppingCartOutlinedIcon />
-                    <Text fontWeight="500" fontSize='md'>Basket</Text>
-                </Link>
+                <Box display="flex" gap="25px" alignItems="center">
+                    <Link display="flex">
+                        <ModeEditOutlineOutlinedIcon />
+                        <Text fontWeight="500" fontSize='md'>Review</Text>
+                    </Link>
+                    <Link display="flex">
+                        <FavoriteBorderOutlinedIcon />
+                        <Text fontWeight="500" fontSize='md'>Trips</Text>
+                    </Link>
+                    <Link display="flex">
+                        <NotificationsOutlinedIcon />
+                        <Text fontWeight="500" fontSize='md'>Alerts</Text>
+                    </Link>
+                    <Link display="flex">
+                        {isAuth ? 
+                            <Button color="black" onClick={() => {
+                                setAuth(!isAuth);
+                            }}>Sanghamitra</Button> : <Signin />
+                        }
+                    </Link>
+                    <Link display="flex">
+                        <ShoppingCartOutlinedIcon />
+                        <Text fontWeight="500" fontSize='md'>Basket</Text>
+                    </Link>
+                </Box>
             </Box>
         </Box>
-    </Box>
+    );
 }
 
 export default Navbar;
