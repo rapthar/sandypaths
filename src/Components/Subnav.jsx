09b0382a525f2daef3591d32e839b4ddfcf76002
Mncedisi Bhembe <rapthargfx@gmail.com>
2024-09-09 14:00:01 +0200

    Update Subnav.jsx

--- src/Components/Subnav.jsx
+++ src/Components/Subnav.jsx
@@ -1,33 +1,36 @@
 import { Box, Menu, MenuButton, MenuItem, MenuList, Portal } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
-import BedroomChildOutlinedIcon from '@mui/icons-material/BedroomChildOutlined';
-import OtherHousesOutlinedIcon from '@mui/icons-material/OtherHousesOutlined';
-import LocalActivityOutlinedIcon from '@mui/icons-material/LocalActivityOutlined';
-import LocalDiningOutlinedIcon from '@mui/icons-material/LocalDiningOutlined';
-import ForumOutlinedIcon from '@mui/icons-material/ForumOutlined';
+import BeachAccessIcon from '@mui/icons-material/BeachAccess';
+import LandscapeIcon from '@mui/icons-material/Landscape';
+import WaterIcon from '@mui/icons-material/Water';
+import DiamondIcon from '@mui/icons-material/Diamond';
+import FilterHdrIcon from '@mui/icons-material/FilterHdr';
+import GrainIcon from '@mui/icons-material/Grain';
+import TerrainIcon from '@mui/icons-material/Terrain';
+import SailingIcon from '@mui/icons-material/Sailing';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 
 function SubNav(){
     return <Box h="100px" w="85%" m="auto" pt="100px" mb="80px" display="grid" gridTemplateColumns="repeat(6, 1fr)" alignItems="center" gap="15px">
         <Box h="50px" _hover={{bg:"black", color:"white"}} borderRadius='xl' p="10px 16px" fontWeight="500" fontSize='md' border="1.5px solid black" justifyContent="space-between" display="flex" alignItems="center">
-            <Link fontSize='md'>Hotels</Link>
-            <BedroomChildOutlinedIcon/>
+            <Link fontSize='md'>Barrier Beaches</Link>
+            <LandscapeIcon />
         </Box>
         <Box h="50px" _hover={{bg:"black", color:"white"}} borderRadius='xl' p="10px 10px" fontWeight="500" fontSize='md' border="1.5px solid black" justifyContent="space-between" display="flex" alignItems="center">
-            <Link fontSize='md'>Holiday Homes</Link>
-            <OtherHousesOutlinedIcon />
+            <Link fontSize='md'>Black Sand Beaches</Link>
+            <GrainIcon />
         </Box>
         <Box h="50px" _hover={{bg:"black", color:"white"}} borderRadius='xl' p="10px 16px" fontWeight="500" fontSize='md' border="1.5px solid black" justifyContent="space-between" display="flex" alignItems="center">
-            <Link fontSize='md'>Things to Do</Link>
-            <LocalActivityOutlinedIcon />
+            <Link fontSize='md'>Cliff Beaches</Link>
+            <TerrainIcon />
         </Box>
         <Box h="50px" _hover={{bg:"black", color:"white"}} borderRadius='xl' p="10px 16px" fontWeight="500" fontSize='md' border="1.5px solid black" justifyContent="space-between" display="flex" alignItems="center">
-            <Link>Restaurants</Link>
-            <LocalDiningOutlinedIcon />
+            <Link>Coral Beaches</Link>
+            <WaterIcon />
         </Box>
         <Box h="50px" _hover={{bg:"black", color:"white"}} borderRadius='xl' p="10px 13px" fontWeight="500" fontSize='md' border="1.5px solid black" justifyContent="space-between" display="flex" alignItems="center">
-            <Link fontSize='md'>Travel Forums</Link>
-            <ForumOutlinedIcon />
+            <Link fontSize='md'>Glass Beaches</Link>
+            <DiamondIcon />
         </Box>
         <Box h="50px" _hover={{bg:"black", color:"white"}} borderRadius='xl' p="10px 16px" fontWeight="500" fontSize='md' border="1.5px solid black" justifyContent="space-between" display="flex" alignItems="center">
             <Menu w="100%">
@@ -38,17 +41,16 @@ function SubNav(){
                 </MenuButton>
                 <Portal>
                     <MenuList color="black" w="80%" m="auto" ml="-15%" >
-                        <MenuItem mt="10px" fontWeight="500" fontSize='17px'><Link>Add a Place</Link></MenuItem>
-                        <MenuItem mt="10px" fontWeight="500" fontSize='17px'><Link>Airlines</Link></MenuItem>
-                        <MenuItem mt="10px" fontWeight="500" fontSize='17px'><Link>Flights</Link></MenuItem>
-                        <MenuItem mt="10px" fontWeight="500" fontSize='17px'><Link>Help Centre</Link></MenuItem>
-                        <MenuItem mt="10px" fontWeight="500" fontSize='17px'><Link>Package Holidays</Link></MenuItem>
-                        <MenuItem mt="10px" fontWeight="500" fontSize='17px'><Link>Travel Forums</Link></MenuItem>
-                        <MenuItem mt="10px" fontWeight="500" fontSize='17px'><Link>Travellers' Choice</Link></MenuItem>
+                        <MenuItem mt="10px" fontWeight="500" fontSize='17px'><Link>Pebble Beaches</Link></MenuItem>
+                        <MenuItem mt="10px" fontWeight="500" fontSize='17px'><Link>Pocket Beaches</Link></MenuItem>
+                        <MenuItem mt="10px" fontWeight="500" fontSize='17px'><Link>Rocky Beaches</Link></MenuItem>
+                        <MenuItem mt="10px" fontWeight="500" fontSize='17px'><Link>Sandy Beaches</Link></MenuItem>
+                        <MenuItem mt="10px" fontWeight="500" fontSize='17px'><Link>Shell Beaches</Link></MenuItem>
                     </MenuList>
                 </Portal>
             </Menu>
         </Box>
     </Box>
 }
+
 export default SubNav;
