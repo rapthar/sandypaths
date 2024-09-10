c99552df3ba95e98da8f9f27597befd31073e158
Mncedisi Bhembe <rapthargfx@gmail.com>
2024-09-10 19:57:10 +0200

    Update Homeseven.jsx

--- src/Components/Home/Homeseven.jsx
+++ src/Components/Home/Homeseven.jsx
@@ -1,44 +1,49 @@
-import { Box, Image, Text } from "@chakra-ui/react";
+import { Box, Image, Text, Flex, Button } from "@chakra-ui/react";
 
-
-function Homeseven(){
-
-    const data= [
-        {
-            id: 1,
-            img: "https://media-cdn.tripadvisor.com/media/vr-splice-j/09/48/dd/9a.jpg",
-            title: "Rentals in Raichak",
-            count: "6 rentals"
-        },
-        {
-            id: 2,
-            img: "https://media-cdn.tripadvisor.com/media/vr-splice-j/06/42/95/5f.jpg",
-            title: "Rentals in Puri",
-            count: "13 rentals"
-        },
-        {
-            id: 3,
-            img: "https://media-cdn.tripadvisor.com/media/vr-splice-j/04/b1/36/b2.jpg",
-            title: "Rentals in Santiniketan",
-            count: "12 rentals"
-        }
-    ]
-
-    return <Box w="85%" m="auto" textAlign="left" mt="50px" mb="50px">
-    <Text fontWeight="700" fontSize='3xl'>Home Rentals Near You</Text>
-    <Box display="grid" justifyContent="space-between" gridTemplateColumns="repeat(4, 1fr)" gap="15px" mt="25px">
-        <Box>
-        <Text fontWeight="700" fontSize='2xl'>We think you'd enjoy these homes for a quick trip out of town.</Text>
+function Homeseven() {
+    return (
+        <Box w="100%" position="relative" h="600px">
+            <Image
+                src="/img/even-hotel-chongli-even.jpg"
+                alt="Beachside Accommodation"
+                objectFit="cover"
+                w="100%"
+                h="100%"
+            />
+            <Box
+                position="absolute"
+                top="0"
+                left="0"
+                right="0"
+                bottom="0"
+                bg="rgba(0,0,0,0.4)"
+            />
+            <Flex
+                position="absolute"
+                top="0"
+                left="0"
+                right="0"
+                bottom="0"
+                direction="column"
+                justify="center"
+                align="flex-start"
+                maxW="6xl"
+                mx="auto"
+                px="4"
+            >
+                <Text color="white" fontSize="5xl" fontWeight="bold" mb="4">
+                    Beachside Bliss
+                </Text>
+                <Text color="white" fontSize="xl" mb="6" maxW="600px">
+                    Experience luxury and comfort with our premium beachside accommodations. 
+                    Wake up to stunning ocean views and fall asleep to the sound of waves.
+                </Text>
+                <Button colorScheme="blue" size="lg">
+                    Explore Accommodations
+                </Button>
+            </Flex>
         </Box>
-        {data && data.map((el)=>{
-            return <Box key={el.id}>
-                <Image h="200px" w="100%" src={el.img} alt="" />
-                <Text fontWeight="700" fontSize='lg'>{el.title}</Text>
-                <Text fontWeight="500" fontSize='md'>{el.count}</Text>
-            </Box>
-        })}
-    </Box>
-</Box>
+    );
 }
 
 export default Homeseven;
