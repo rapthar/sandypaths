26ab754ae39e619da9bce7726225499ee7df4db6
Mncedisi Bhembe <rapthargfx@gmail.com>
2024-09-10 13:35:33 +0200

    Add files via upload

--- src/Components/Home/Homesix.jsx
+++ src/Components/Home/Homesix.jsx
@@ -1,38 +1,48 @@
-import { Box, Heading, Input, Button, Flex } from "@chakra-ui/react";
+import { Box, Heading, Input, Button, Flex, Text } from "@chakra-ui/react";
 
 function Homesix() {
   return (
-    <Box
-      w="100%"
-      bgImage="url('/img/image-3.jpg')"
-      bgSize="cover"
-      bgPosition="center"
-      py="60px"
-    >
-      <Box maxW="6xl" mx="auto" px="4">
-        <Flex direction="column" align="center">
-          <Heading as="h2" size="2xl" color="white" mb="4" textAlign="center">
-            Subscribe
-          </Heading>
-          <Heading as="h3" size="md" color="white" mb="8" textAlign="center">
-            & Stay updated
-          </Heading>
-          <Flex w="100%" maxW="800px" gap="4">
-            <Input
-              placeholder="Your name"
-              bg="white"
-              size="lg"
-              borderRadius="full"
-            />
+    <Box maxW="6xl" mx="auto" px="4" py="8">
+      <Box
+        borderRadius="xl"
+        overflow="hidden"
+        position="relative"
+      >
+        <Box
+          bgImage="url('/img/image-3.jpg')"
+          bgSize="cover"
+          bgPosition="center"
+          filter="brightness(50%)"
+          position="absolute"
+          top="0"
+          left="0"
+          right="0"
+          bottom="0"
+        />
+        <Flex direction={{ base: "column", md: "row" }} align="center" justify="space-between" p="8" position="relative">
+          <Box mb={{ base: "6", md: "0" }} mr={{ md: "6" }}>
+            <Heading as="h2" size="xl" color="white" mb="2">
+              Subscribe
+            </Heading>
+            <Text color="white" fontSize="lg">
+              & Stay updated
+            </Text>
+          </Box>
+          <Flex 
+            w={{ base: "100%", md: "auto" }} 
+            maxW="500px" 
+            bg="white" 
+            borderRadius="full" 
+            overflow="hidden"
+          >
             <Input
               placeholder="Your email"
-              bg="white"
-              size="lg"
-              borderRadius="full"
+              border="none"
+              _focus={{ boxShadow: "none" }}
+              flex="1"
             />
             <Button
               colorScheme="red"
-              size="lg"
               borderRadius="full"
               px="8"
             >
