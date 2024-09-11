46a3dc2166920fc72ee32fbaf78937b4ff27baa9
Mncedisi Bhembe <rapthargfx@gmail.com>
2024-09-11 16:37:26 +0200

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -8,13 +8,13 @@ import Homeseven from '../Components/Home/Homeseven'
 import Homesix from '../Components/Home/Homesix'
 import Homethree from '../Components/Home/Homethree'
 import Hometwo from '../Components/Home/Hometwo'
-import Hometwo from '../Components/Home/Homenine'
+import Homenine from '../Components/Home/Homenine'
 
 const Home = () => {
   return (
     <Box>
       <Homeone />
-      <Homenine/>
+      <Homenine />
       <Hometwo />
       <Homethree />
       <Homefour />
