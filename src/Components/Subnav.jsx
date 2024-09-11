278f738c3cb3ebf5ce4f5aafe73c90efe901d180
Mncedisi Bhembe <rapthargfx@gmail.com>
2024-09-11 16:04:11 +0200

    Update Subnav.jsx

--- src/Components/Subnav.jsx
+++ src/Components/Subnav.jsx
@@ -1,34 +1,36 @@
 import { Box, Menu, MenuButton, MenuItem, MenuList, Portal } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
-import WavesIcon from '@mui/icons-material/Waves';
-import WaterIcon from '@mui/icons-material/Water';
-import LakeIcon from '@mui/icons-material/Water';
-import RiverIcon from '@mui/icons-material/Waves';
 import BeachAccessIcon from '@mui/icons-material/BeachAccess';
+import LandscapeIcon from '@mui/icons-material/Landscape';
+import WaterIcon from '@mui/icons-material/Water';
+import DiamondIcon from '@mui/icons-material/Diamond';
+import FilterHdrIcon from '@mui/icons-material/FilterHdr';
+import GrainIcon from '@mui/icons-material/Grain';
+import TerrainIcon from '@mui/icons-material/Terrain';
 import SailingIcon from '@mui/icons-material/Sailing';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 
 function SubNav(){
     return <Box h="100px" w="85%" m="auto" pt="100px" mb="80px" display="grid" gridTemplateColumns="repeat(6, 1fr)" alignItems="center" gap="15px">
         <Box h="50px" _hover={{bg:"#193F50", color:"white"}} borderRadius='xl' p="10px 16px" fontWeight="500" fontSize='md' border="1.5px solid #193F50" justifyContent="space-between" display="flex" alignItems="center">
-            <Link fontSize='md'>Oceans</Link>
-            <WavesIcon />
+            <Link fontSize='md'>Barrier Beach</Link>
+            <LandscapeIcon />
+        </Box>
+        <Box h="50px" _hover={{bg:"#193F50", color:"white"}} borderRadius='xl' p="10px 10px" fontWeight="500" fontSize='md' border="1.5px solid #193F50" justifyContent="space-between" display="flex" alignItems="center">
+            <Link fontSize='md'>Black Sand Beach</Link>
+            <GrainIcon />
         </Box>
         <Box h="50px" _hover={{bg:"#193F50", color:"white"}} borderRadius='xl' p="10px 16px" fontWeight="500" fontSize='md' border="1.5px solid #193F50" justifyContent="space-between" display="flex" alignItems="center">
-            <Link fontSize='md'>Seas</Link>
+            <Link fontSize='md'>Cliff Beach</Link>
+            <TerrainIcon />
+        </Box>
+        <Box h="50px" _hover={{bg:"#193F50", color:"white"}} borderRadius='xl' p="10px 16px" fontWeight="500" fontSize='md' border="1.5px solid #193F50" justifyContent="space-between" display="flex" alignItems="center">
+            <Link>Coral Beach</Link>
             <WaterIcon />
         </Box>
-        <Box h="50px" _hover={{bg:"#193F50", color:"white"}} borderRadius='xl' p="10px 16px" fontWeight="500" fontSize='md' border="1.5px solid #193F50" justifyContent="space-between" display="flex" alignItems="center">
-            <Link fontSize='md'>Lakes</Link>
-            <LakeIcon />
-        </Box>
-        <Box h="50px" _hover={{bg:"#193F50", color:"white"}} borderRadius='xl' p="10px 16px" fontWeight="500" fontSize='md' border="1.5px solid #193F50" justifyContent="space-between" display="flex" alignItems="center">
-            <Link>Rivers</Link>
-            <RiverIcon />
-        </Box>
-        <Box h="50px" _hover={{bg:"#193F50", color:"white"}} borderRadius='xl' p="10px 16px" fontWeight="500" fontSize='md' border="1.5px solid #193F50" justifyContent="space-between" display="flex" alignItems="center">
-            <Link fontSize='md'>Beaches</Link>
-            <BeachAccessIcon />
+        <Box h="50px" _hover={{bg:"#193F50", color:"white"}} borderRadius='xl' p="10px 13px" fontWeight="500" fontSize='md' border="1.5px solid #193F50" justifyContent="space-between" display="flex" alignItems="center">
+            <Link fontSize='md'>Glass Beach</Link>
+            <DiamondIcon />
         </Box>
         <Box h="50px" _hover={{bg:"#193F50", color:"white"}} borderRadius='xl' p="10px 16px" fontWeight="500" fontSize='md' border="1.5px solid #193F50" justifyContent="space-between" display="flex" alignItems="center">
             <Menu w="100%">
@@ -39,11 +41,11 @@ function SubNav(){
                 </MenuButton>
                 <Portal>
                     <MenuList color="#193F50" w="80%" m="auto" ml="-15%" >
-                        <MenuItem mt="10px" fontWeight="500" fontSize='17px'><Link>Bays</Link></MenuItem>
-                        <MenuItem mt="10px" fontWeight="500" fontSize='17px'><Link>Lagoons</Link></MenuItem>
-                        <MenuItem mt="10px" fontWeight="500" fontSize='17px'><Link>Estuaries</Link></MenuItem>
-                        <MenuItem mt="10px" fontWeight="500" fontSize='17px'><Link>Coves</Link></MenuItem>
-                        <MenuItem mt="10px" fontWeight="500" fontSize='17px'><Link>Sand Bars</Link></MenuItem>
+                        <MenuItem mt="10px" fontWeight="500" fontSize='17px'><Link>Pebble Beach</Link></MenuItem>
+                        <MenuItem mt="10px" fontWeight="500" fontSize='17px'><Link>Pocket Beach</Link></MenuItem>
+                        <MenuItem mt="10px" fontWeight="500" fontSize='17px'><Link>Rocky Beach</Link></MenuItem>
+                        <MenuItem mt="10px" fontWeight="500" fontSize='17px'><Link>Sandy Beach</Link></MenuItem>
+                        <MenuItem mt="10px" fontWeight="500" fontSize='17px'><Link>Shell Beach</Link></MenuItem>
                     </MenuList>
                 </Portal>
             </Menu>
