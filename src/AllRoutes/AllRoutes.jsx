6ff6dfa06519c074aa54679ea090ec9fc094fd4d
Mncedisi Bhembe <rapthargfx@gmail.com>
2024-09-11 21:15:39 +0200

    About

--- src/AllRoutes/AllRoutes.jsx
+++ src/AllRoutes/AllRoutes.jsx
@@ -1,11 +1,13 @@
 import {Routes, Route} from "react-router-dom";
 import Home from "../Pages/Home";
 import Hotel from "../Pages/Hotel";
+import Hotel from "../Pages/About";
 
 function AllRoutes(){
     return <Routes>
         <Route path="/" element={<Home />} ></Route>
         <Route path="/hotel" element={<Hotel />} ></Route>
+        <Route path="/about" element={<About />} ></Route>
     </Routes>
 }
 
