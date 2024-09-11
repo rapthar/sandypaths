8a7bcaf03e52fa7a2d5e8601fc9262620cee50c2
Mncedisi Bhembe <rapthargfx@gmail.com>
2024-09-11 19:59:12 +0200

    Update Hometen.jsx

--- src/Components/Home/Hometen.jsx
+++ src/Components/Home/Hometen.jsx
@@ -26,8 +26,8 @@ const SideCard = ({ title, description, image }) => (
   <HStack spacing={4} align="start">
     <Image src={image} alt={title} objectFit="cover" w="180px" h="120px" borderRadius="xl" />
     <VStack align="start" spacing={1} flex={1}>
-      <Heading size="md">{title}</Heading>
-      <Text fontSize="sm" color="gray.600" noOfLines={2}>{description}</Text>
+      <Heading size="md" textAlign="left">{title}</Heading>
+      <Text fontSize="sm" color="gray.600" noOfLines={2} textAlign="left">{description}</Text>
       <Link href="#" fontSize="sm" fontWeight="bold" display="inline-flex" alignItems="center">
         READ MORE <ChevronRightIcon ml={1} />
       </Link>
@@ -39,20 +39,16 @@ const TravelBlogSection = () => {
   return (
     <Box bg="white" py={8}>
       <Box maxWidth="1200px" margin="auto" px={4}>
-        <VStack align="start" spacing={4} mb={8}>
-          <Heading size="2xl" fontWeight="bold">Read Our Latest Travel Blog & Tips Here</Heading>
-          <Text color="gray.600">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et.</Text>
-        </VStack>
-        
-        <Grid templateColumns={{ base: "1fr", lg: "3fr 2fr" }} gap={8}>
+        <Grid templateColumns={{ base: "1fr", lg: "3fr 2fr" }} gap={8} alignItems="start">
           <VStack align="stretch" spacing={4}>
-            <Heading size="xl">Tropical Bliss: Rejuvenate in Asia's Idyllic Beaches</Heading>
             <MainCard 
-              title=""  // Title moved outside
+              title="Tropical Bliss: Rejuvenate in Asia's Idyllic Beaches"
               image="/img/romantic.jpg"
             />
           </VStack>
           <VStack spacing={8} align="stretch">
+            <Heading size="2xl" fontWeight="bold">Read Our Latest Travel Blog & Tips Here</Heading>
+            <Text color="gray.600">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et.</Text>
             <SideCard 
               title="Asia's Thrilling Adventures: Hiking, Trekking, and More" 
               description="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo..." 
