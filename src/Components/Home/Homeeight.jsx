9c1b7db9fa0beba9446adebf7d2de21b509c4486
Mncedisi Bhembe <rapthargfx@gmail.com>
2024-09-11 08:42:29 +0200

    Update Homeeight.jsx

--- src/Components/Home/Homeeight.jsx
+++ src/Components/Home/Homeeight.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Box, Heading, Text, Flex, Image } from "@chakra-ui/react"
+import { Box, Heading, Text, Flex, Image, Icon } from "@chakra-ui/react"
+import { FaFacebookF, FaInstagram, FaTwitter, FaTiktok } from 'react-icons/fa'
 
 const images = [
   { src: '/img/family-friendly.jpg', credit: '@richardwhitetravels' },
@@ -12,21 +13,26 @@ const images = [
 
 function Homeeight() {
   return (
-    <Box bg="blue.500" w="100%" py={8}>
+    <Box bg="blue.500" pt="30px" w="100%" py={8}>
       <Box maxW="6xl" mx="auto">
         <Flex justify="space-between" align="center" mb={6}>
           <Heading as="h2" size="2xl" color="white">
-            #lonelyplanet
+            #sandypaths
           </Heading>
           <Flex align="center">
-            <Text color="white" mr={4}>FOLLOW LONELY PLANET:</Text>
-            {/* Add social media icons here */}
+            <Text color="white" mr={4}>FOLLOW SANDYPATHS:</Text>
+            <Flex>
+              <Icon as={FaFacebookF} color="white" w={6} h={6} mr={2} />
+              <Icon as={FaInstagram} color="white" w={6} h={6} mr={2} />
+              <Icon as={FaTwitter} color="white" w={6} h={6} mr={2} />
+              <Icon as={FaTiktok} color="white" w={6} h={6} />
+            </Flex>
           </Flex>
         </Flex>
         <Flex overflowX="auto" pb={4}>
           {images.map((image, index) => (
             <Box key={index} minW="300px" mr={4} position="relative">
-              <Image src={image.src} alt={`Travel image ${index + 1}`} w="300px" h="200px" objectFit="cover" />
+              <Image src={image.src} alt={`Travel image ${index + 1}`} w="300px" h="300px" objectFit="cover" />
               <Text position="absolute" bottom={2} left={2} color="white" fontSize="sm">
                 {image.credit}
               </Text>
