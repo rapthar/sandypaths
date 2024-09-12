5e81cd5e1652e418bae1ce36e418aa3ba22f3142
Mncedisi Bhembe <rapthargfx@gmail.com>
2024-09-12 12:22:40 +0200

    Update Homeone.jsx

--- src/Components/Home/Homeone.jsx
+++ src/Components/Home/Homeone.jsx
@@ -1,95 +1,65 @@
-import React from 'react';
-import { Box, Button, Flex, Heading, Text, VStack } from "@chakra-ui/react";
+import React from 'react'
+import { Box, Heading, Text, Button, Flex, Container } from "@chakra-ui/react"
+import { ArrowForwardIcon } from "@chakra-ui/icons"
 
 function Homeone() {
   return (
-    <Box borderRadius="xl" overflow="hidden" bg="#FFF5EE" p={4}>
-      <Flex borderRadius="xl" overflow="hidden" bg="#E27B60" position="relative">
-        {/* Left column */}
-        <Box width="50%" p={8} color="white">
-          <VStack align="flex-start" spacing={4}>
-            <Heading as="h1" size="2xl" fontWeight="bold" lineHeight={1.2}>
+    <Container maxW="container.xl" py={8}>
+      <Box
+        borderRadius="2xl"
+        overflow="hidden"
+        position="relative"
+        height="400px"
+        bg="linear-gradient(to right, #e17055, #fab1a0)"
+      >
+        <Flex height="100%">
+          {/* Left side content */}
+          <Box
+            width="50%"
+            p={12}
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+            color="white"
+          >
+            <Heading as="h1" size="3xl" fontWeight="bold" mb={4} lineHeight="1.2">
               Explore our Wondersome Adventures
             </Heading>
-            <Text fontSize="md">
+            <Text fontSize="xl" mb={6}>
               Save up to 50% – book your dream trip now!
             </Text>
             <Button
-              bg="white"
-              color="#E27B60"
-              size="md"
-              borderRadius="md"
-              fontWeight="bold"
-              _hover={{ bg: "gray.100" }}
+              rightIcon={<ArrowForwardIcon />}
+              colorScheme="white"
+              variant="outline"
+              alignSelf="flex-start"
+              _hover={{ bg: 'white', color: '#e17055' }}
             >
-              Book Now →
+              Book Now
             </Button>
-          </VStack>
-        </Box>
+          </Box>
 
-        {/* Curved divider */}
+          {/* Right side image */}
+          <Box
+            width="50%"
+            backgroundImage="url('src/Components/Home/Homefour.jsx')"
+            backgroundSize="cover"
+            backgroundPosition="center"
+          />
+        </Flex>
+
+        {/* Curved overlay */}
         <Box
           position="absolute"
           top={0}
-          bottom={0}
           left="45%"
-          width="10%"
-          bg="#E27B60"
-          zIndex={1}
-        >
-          <Box
-            position="absolute"
-            top={0}
-            bottom={0}
-            right={0}
-            width="100%"
-            bg="#FFF5EE"
-            borderTopLeftRadius="full"
-            borderBottomLeftRadius="full"
-          />
-        </Box>
-
-        {/* Right column (image placeholder) */}
-        <Box width="50%" position="relative">
-          <Box
-            position="absolute"
-            top={0}
-            right={0}
-            bottom={0}
-            left={0}
-            bg="url('/img/private.jpg')"
-            bgSize="cover"
-            bgPosition="center"
-          />
-        </Box>
-      </Flex>
-
-      {/* Decorative lines */}
-      <Box
-        position="absolute"
-        top={0}
-        left={0}
-        right={0}
-        bottom={0}
-        opacity={0.1}
-        pointerEvents="none"
-      >
-        {[...Array(5)].map((_, i) => (
-          <Box
-            key={i}
-            position="absolute"
-            top={`${20 * i}%`}
-            left={0}
-            width="100%"
-            height="1px"
-            bg="white"
-            transform={`rotate(${5 + i * 2}deg)`}
-            transformOrigin="left"
-          />
-        ))}
+          right={0}
+          bottom={0}
+          bg="linear-gradient(to left bottom, transparent 50%, #fab1a0 50%)"
+        />
       </Box>
-    </Box>
-  );
+    </Container>
+  )
 }
 
-export default Homeone;
+export default Homeone
