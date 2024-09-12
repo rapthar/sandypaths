fe8ab46ca413156d3c7378ee9f3e451c14de1c3c
Mncedisi Bhembe <rapthargfx@gmail.com>
2024-09-12 11:43:21 +0200

    Update Homeone.jsx

--- src/Components/Home/Homeone.jsx
+++ src/Components/Home/Homeone.jsx
@@ -1,62 +1,61 @@
 import { SearchIcon } from "@chakra-ui/icons";
-import { Box, Input, Heading, Text } from "@chakra-ui/react";
+import { Box, Input, Heading, Text, VStack, Container, Flex } from "@chakra-ui/react";
 
 function Homeone() {
-	return (
-		<Box 
-			w="85%" 
-			m="auto" 
-			p="150px 100px" 
-			position="relative"
-			backgroundImage='url(https://static.tacdn.com/img2/brand/home/homemar2022_dt_trans.webp)'
-			backgroundSize="cover"
-			backgroundPosition="center"
-		>
-			{/* Black overlay */}
-			<Box 
-				position="absolute"
-				top="0"
-				left="0"
-				right="0"
-				bottom="0"
-				bg="rgba(0, 0, 0, 0.5)"
-			/>
+  return (
+    <Box position="relative" height="100vh" overflow="hidden">
+      {/* Background Image */}
+      <Box
+        position="absolute"
+        top="0"
+        left="0"
+        right="0"
+        bottom="0"
+        backgroundImage="url(@hidden-gem.jpg)"
+        backgroundSize="cover"
+        backgroundPosition="center"
+        filter="brightness(0.7)"
+      />
 
-			{/* Content */}
-			<Box position="relative" zIndex="1">
-				<Heading
-					as="h1"
-					size="2xl"
-					color="white"
-					mb={4}
-					textAlign="center"
-				>
-					Discover Your Perfect Beach
-				</Heading>
-				<Text
-					fontSize="xl"
-					color="white"
-					mb={8}
-					textAlign="center"
-				>
-					Explore the world's most beautiful coastlines with SandyPaths
-				</Text>
-				<Box 
-					display="flex" 
-					rounded="40px" 
-					bg="white" 
-					p="8px 20px" 
-					w="85%" 
-					m="auto" 
-					alignItems="center" 
-					boxShadow='3xl'
-				>
-					<SearchIcon w={5} h={5} color="#193F50" />
-					<Input border="none" placeholder='Where to?' size='lg' />
-				</Box>
-			</Box>
-		</Box>
-	)
+      {/* Content Overlay */}
+      <Container maxW="container.xl" height="100%" position="relative">
+        <Flex height="100%" alignItems="center" justifyContent="space-between">
+          {/* Left side content */}
+          <VStack align="flex-start" spacing={6} maxW="50%" color="white">
+            <Heading as="h1" size="3xl" fontWeight="bold">
+              Discover Your Perfect Beach
+            </Heading>
+            <Text fontSize="xl">
+              Explore the world's most beautiful coastlines with SandyPaths
+            </Text>
+            
+            {/* Search Form */}
+            <Box 
+              display="flex" 
+              rounded="full" 
+              bg="white" 
+              p="8px 20px" 
+              w="100%" 
+              alignItems="center" 
+              boxShadow='xl'
+            >
+              <SearchIcon w={5} h={5} color="#193F50" />
+              <Input 
+                border="none" 
+                placeholder='Where to?' 
+                size='lg' 
+                _placeholder={{ color: 'gray.500' }}
+                _focus={{ boxShadow: 'none' }}
+              />
+            </Box>
+          </VStack>
+
+          {/* Right side - empty space to match layout */}
+          <Box flexBasis="40%" />
+        </Flex>
+      </Container>
+    </Box>
+  );
 }
 
 export default Homeone;
