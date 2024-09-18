e8e0da0922a09dd731ce094a848bfd1aa09e41bc
Mncedisi Bhembe <rapthargfx@gmail.com>
2024-09-18 23:32:54 +0200

    Update AdminDashboard.jsx

--- src/Components/AdminDashboard.jsx
+++ src/Components/AdminDashboard.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react'
 import { PlusCircle, Search, Home, FileText, Tag, Droplet, Settings, Umbrella, MapPin, Building, Map, BarChart, ChevronDown, Waves, Palmtree, Users, Clock, ArrowUpDown } from 'lucide-react'
-import { palmTree, droplets, waves, ship, fish, anchor, mountain, umbrella } from 'lucide-react';
+import { palmtree-island-sun, droplets, waves, ship, fish, anchor, mountain, umbrella } from 'lucide-react';
 
 const waterBodyTypes = [
-  { name: "Beaches", icon: palmTree },
+  { name: "Beaches", icon: palmtree-island-sun },
   { name: "Lakes", icon: droplets },
   { name: "Rivers", icon: waves },
   { name: "Bays", icon: ship },
